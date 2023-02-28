569dac4cc54555c9f22d66ef42c7d83af45d0b7a
NguyenDangKhoa3118410198 <71954389+NguyenDangKhoa3118410198@users.noreply.github.com>
2023-02-28 22:14:29 +0700

    add imput search

--- src/components/Layouts/componentsLayout/Header/index.js
+++ src/components/Layouts/componentsLayout/Header/index.js
@@ -1,11 +1,36 @@
 import classNames from "classnames/bind";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+   faCircleXmark,
+   faSpinner,
+   faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 import styles from "./Header.module.scss";
 
 const cx = classNames.bind(styles); // dung de co the su dung -  trong ten, vi du (post-item)
 function Header() {
    return (
       <header className={cx("wrapper")}>
-         <div className={cx("inner")}>LOGO</div>
+         <div className={cx("inner")}>
+            <div className={cx("logo")}>
+               <h1>KOSSPRZ</h1>
+            </div>
+
+            <div className={cx("search")}>
+               <input placeholder="Search something..." spellcheck={false} />
+               <button className={cx("clear")}>
+                  <FontAwesomeIcon icon={faCircleXmark} />
+               </button>
+
+               <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+
+               <button className={cx("search-btn")}>
+                  <FontAwesomeIcon icon={faMagnifyingGlass} />
+               </button>
+            </div>
+
+            <div className={cx("actions")}></div>
+         </div>
       </header>
    );
 }
